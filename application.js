3b28a1ab64b0b67eac74a2e52a72e4fa437b7ef8
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-03 10:35:06 -0500

    adds prep table and batch array

--- application.js
+++ application.js
@@ -1,6 +1,18 @@
+var batchArray = []
+
 var Batch = function(batch_type, bake_time){
   this.batch_type = batch_type
   this.bake_time = bake_time
+  this.doneness = function(doneness){
+    this.status = doneness || "raw"
+  }
+}
+
+var prepTable = {
+  append: function(batch) {
+    batchArray.push(batch)
+    console.log(batchArray)
+  }
 }
 
 var bakeryForm = {
@@ -10,7 +22,7 @@ var bakeryForm = {
       var type = $(this).find("input[name=batch_type]").val()
       var time = $(this).find("input[name=bake_time]").val()
       batch = new Batch(type, time)
-  
+      prepTable.append(batch)
     })
   }
 }
