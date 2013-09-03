3553ec8c9d0a6aac8435e7e5d5d0510c267509a2
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-03 10:10:51 -0500

    adds batch constructor and get form function

--- application.js
+++ application.js
@@ -1,3 +1,20 @@
+var Batch = function(batch_type, bake_time){
+  this.batch_type = batch_type
+  this.bake_time = bake_time
+}
+
+var bakeryForm = {
+  get: function() {
+    $("#new_batch").on("submit", function(e){
+      e.preventDefault();
+      var type = $(this).find("input[name=batch_type]").val()
+      var time = $(this).find("input[name=bake_time]").val()
+      batch = new Batch(type, time)
+  
+    })
+  }
+}
+
 $(document).ready(function() {
-  console.log("from the mid");
+  bakeryForm.get()
 });
